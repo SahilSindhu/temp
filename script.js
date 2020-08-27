7db7450c4dec3cd4145acf7ef227860cfe6db93c
Sahil Sindhu <sahilsindhu16@gmail.com>
2020-08-27 09:48:10 +0530

    none

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function () {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
   const firstDate = new Date(2020, 8, 29);
-  const secondDate = new Date(2020, 8, 26);
+  const secondDate = new Date(2020, 8, 28);
 
   const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
   document.querySelector(
